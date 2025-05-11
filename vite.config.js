acc69fcba894b1637b7727614bd625c7acd095fc
Minhihik4 <minhminh9956@gmail.com>
2025-05-12 00:36:01 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/WEB/",
+  base: "/FE/",
   plugins: [react()],
   server: {
     proxy: {
